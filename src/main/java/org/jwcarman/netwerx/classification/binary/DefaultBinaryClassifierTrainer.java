c74e30a229cc078e83b1d437819e2a3a48b58e1a
James Carman <jcarman@callibrity.com>
2025-05-04 01:36:31 -0400

    Removing unused fields.

--- src/main/java/org/jwcarman/netwerx/classification/binary/DefaultBinaryClassifierTrainer.java
+++ src/main/java/org/jwcarman/netwerx/classification/binary/DefaultBinaryClassifierTrainer.java
@@ -9,9 +9,6 @@ public class DefaultBinaryClassifierTrainer<M extends Matrix<M>> implements Bina
 
 // ------------------------------ FIELDS ------------------------------
 
-    private static final double TRUE = 1.0;
-    private static final double FALSE = 0.0;
-
     private final NeuralNetworkTrainer<M> networkTrainer;
 
 // --------------------------- CONSTRUCTORS ---------------------------
